3ca9e79d099f54e445f14ade5d2226965b3c1662
Sixtus Agbo <miracleagbosixtus@gmail.com>
2025-01-17 15:56:01 +0100

    Use foundry test cheatcodes

--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -9,9 +9,14 @@ import {DeployFundMe} from "../script/DeployFundMe.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether; // 100000000000000000
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -30,5 +35,18 @@ contract FundMeTest is Test {
             uint256 version = fundMe.getVersion();
             assertEq(version, 6);
         }
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // the next line should revert
+        fundMe.fund(); // send 0 value
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
