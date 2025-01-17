e70c8ba77f162936c0b2b3c8e37eb7fa54bfbb45
Sixtus Agbo <miracleagbosixtus@gmail.com>
2025-01-17 12:52:07 +0100

    Use mock contract to get anvil eth config

--- foundry-fund-me/script/HelperConfig.s.sol
+++ foundry-fund-me/script/HelperConfig.s.sol
@@ -7,6 +7,7 @@
 
 pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     // If we're on a local anvil, we deploy mocks
@@ -43,7 +44,19 @@ contract HelperConfig is Script {
         return ethConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
         // price feed address
+
+        // 1. Deploy mocks
+        // 2. Return mock address
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+
+        return anvilConfig;
     }
 }
