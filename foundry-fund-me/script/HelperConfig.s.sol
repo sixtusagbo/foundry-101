c8d1572e26ff0912e58a4f8a3075b7e37f8faee0
Sixtus Agbo <miracleagbosixtus@gmail.com>
2025-01-17 10:57:45 +0100

    Setup local env for testing and run tests on forked chains

---
+++ foundry-fund-me/script/HelperConfig.s.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+
+// 1. Deploy mocks when we are on a local anvil chain
+// 2. Keep track of different contract addresses across different chains
+// Sepolia ETH/USD
+// Mainnet ETH/USD
+
+pragma solidity ^0.8.18;
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    // If we're on a local anvil, we deploy mocks
+    // Otherwise, grab the existing address from the live network
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return ethConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+    }
+}
